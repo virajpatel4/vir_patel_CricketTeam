e14f7208ed8df30b49284380ea7bbbb67106afdd
virajpatel4 <VP97@myscc.ca>
2023-04-06 22:40:04 -0400

    assignment 8

--- src/app/content-card/content-card.component.ts
+++ src/app/content-card/content-card.component.ts
@@ -1,24 +1,30 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
+import { EntertainmentServiceService } from '../entertainment-service.service';
 
 @Component({
   selector: 'app-content-card',
-  templateUrl: './content-card.component.html',
-  styleUrls: ['./content-card.component.css']
+  templateUrl: '../content-card/content-card.component.html',
+  styleUrls: ['../content-card//content-card.component.css']
 })
-export class ContentCardComponent implements OnInit {
-
-  @Input()
-  content!: Content;
-
-  constructor() {
-    
+export class ContentCardComponent{
+  @Input() contentToDisplay:Content | any;
+content: any;
+ 
+  constructor(private entrnService: EntertainmentServiceService){
+   
   }
 
-  ngOnInit(){
+  logIdTitle(contentInfoToLog:Content):void{
+    console.log(`ID: ${contentInfoToLog.id}`)
+    console.log(`TITLE: ${contentInfoToLog.title}`)
   }
 
-  click(id: number) {
-    console.log(id);
+  addNewContentToList(event:Content){
+    const contentIndxToUpdate = this.entrnService.sharedContent.findIndex(content=>content.id==event.id);
+    if (contentIndxToUpdate > -1) {
+      this.entrnService.sharedContent[contentIndxToUpdate] = event;
+      this.entrnService.sharedContent = [...this.entrnService.sharedContent];
+    }
   }
 }
--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -86,3 +86,4 @@ export class ContentListComponent {
     this.contents.push(content);
   }
 }
+
